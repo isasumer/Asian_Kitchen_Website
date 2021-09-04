ca5757e2a210412e9cbbdb261d6eb73df2642f6a
Isa SUMER <sumerisa5@gmail.com>
2021-09-04 12:04:31 +0300

    menu created

--- app.js
+++ app.js
@@ -81,9 +81,48 @@ const menu = [
       desc: `Red bean paste dessert, serving with honey.`,
     },
   ];
-  let list= ["All", "Korea", "Japan", "China"]
+
+  const section = document.querySelector(".section-center");
+
+
+  let list= menu.reduce (
+    (values,item) => {
+      if (!values.includes(item.category)) {
+      values.push(item.category)
+    }
+    return values;
+  }, ["All"]
+  )
   list.forEach(element => {
   let btn = document.createElement("button");
   btn.setAttribute("class", "btn-item");
 document.getElementById("btn-container").appendChild(btn);
 btn.innerHTML=`${element}`})
+
+
+
+const menuList = (menuItems) => {
+  let displayMenu = menuItems.map((item) => {
+    return `<div class="menu-items col-lg-6 col-sm-12">
+            <img
+              src=${item.img}
+              alt=${item.title}
+              class="photo"
+            />
+            <div class="menu-info">
+              <div class="menu-title">
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+              </div>
+              <div class="menu-text">
+                ${item.desc}
+              </div>
+            </div>
+          </div>
+    `;
+  });
+  displayMenu = displayMenu.join("");
+  section.innerHTML = displayMenu;
+};
+
+menuList(menu);
